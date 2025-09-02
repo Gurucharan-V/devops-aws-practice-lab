16753a073081bcb5f4991b265eedfcbb0a09db00
Gurucharan <72401652+Gurucharan-V@users.noreply.github.com>
2025-09-01 20:36:22 -0400

    push all

--- first.sh
+++ first.sh
@@ -1,5 +1,31 @@
-#!/bin/bash
+# #!/bin/bash
 
-#comments
+# #comments
 
-echo "hey there"
+# PERSON1=$1
+# SAI=$2
+
+# echo "hey there"
+# echo "ela unnaru"
+# echo "em doing"
+
+# BABA=${PERSON1}+$SAI
+
+# echo $BABA
+
+# echo "enter username" 
+# read USERNAME
+# echo "enter passkey"
+# read -s PASS
+
+# echo "user: ${USERNAME} and password: ${PASS}"
+
+
+NUM1=$1
+NUM2=$2
+
+TIMESTAMP=$(date +"%Y-%m-%d")
+echo "executed at $TIMESTAMP"
+SUM=$(($NUM1+$NUM2))
+
+echo "$SUM"
